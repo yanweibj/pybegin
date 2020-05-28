f17922a8679d2824bf47aa5c5acd0081684c97dd
wei yan <yanweibj@cn.ibm.com>
2020-05-28 11:02:53 +0800

    update homework 3-1
    update homework 3-1

--- homework/lesson-3/homework3-1.py
+++ homework/lesson-3/homework3-1.py
@@ -6,17 +6,15 @@
 float_height = 100.0
 
 #设定初始距离为0
-float_sum= 0.0
+float_sum= float_height
 #设定初始弹起次数为0
 int_count = 0
 #当小球高度大于0.01米时进行循环
-while float_height >= 0.01:
+while float_height/2 >= 0.01:
     int_count += 1
-    float_sum += float_height
     float_height /= 2
-    float_sum += float_height
+    float_sum += float_height * 2
     print("第%d次弹起来的高度是%f." % (int_count, float_height))
-#加入最后一次落地的高度
-float_sum += float_height
+
 print("一共弹起%d次" %int_count)
 print("总共走了%.2f米" % float_sum)
